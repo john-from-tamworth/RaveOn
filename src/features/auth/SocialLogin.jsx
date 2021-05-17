8b6d2776bfe6b5477b32bda059baedce66607d5c
John Hands <johnhands@Johns-MacBook-Pro.local>
2021-05-17 23:38:07 +0100

    facebook login added, however only works in dev mode as we will need a privacy policy agreed by facebook for this to work in a live deployed app

--- src/features/auth/SocialLogin.jsx
+++ src/features/auth/SocialLogin.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Button } from 'semantic-ui-react';
+import { closeModal } from '../../app/common/modals/modalReducer';
+import { socialLogin } from '../../app/firestore/firebaseService';
 
 function SocialLogin() {
+  const dispatch = useDispatch();
+
+  function handleSocialLogin(provider) {
+    dispatch(closeModal());
+    socialLogin(provider);
+  }
+
   return (
     <>
       <Button
+        onClick={() => handleSocialLogin('facebook')}
         icon='facebook'
         fluid
         color='facebook'
@@ -12,6 +23,7 @@ function SocialLogin() {
         content='Login with Facebook'
       />
       <Button
+        onClick={() => handleSocialLogin('google')}
         icon='google'
         fluid
         color='google plus'
